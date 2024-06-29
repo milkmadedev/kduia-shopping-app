9da2998fa88cbf3fbb8956115cb5240973ba4c39
milkmade <m@milkmade.dev>
2024-06-28 23:35:45 -0500

    changes made from theia lab enviroment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
@@ -8,8 +8,26 @@ import Location from './components/Location';
 
 function App() {
   return (
-    <div className="App">
+    <AppProvider>
+    <div className="App">'
+    
+      <h1 className="fHead">My Shopping :</h1>
+      <div className="AppRow">
+        <div className='col-sm'>
+          <CartValue />
+        </div>
+        <div className='col-sm'>
+          <ExpenseList/>
+        </div>
+        <div className='col-sm'>
+          <ItemSelected />
+        </div>
+        <div className='col-sm'>
+          <Location />
+        </div>
+      </div>
     </div>
+    </AppProvider>
   );
 }
 
